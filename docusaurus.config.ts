c483b60bc950509c43ebfd8dd66ae58ad81d804e
nazarov <nezerovabuzar@gmail.com>
2026-04-07 22:24:48 +0400

    fix: base url chagned to 2flex

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
     url: "https://2flexible.com",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/",
+    baseUrl: "/2flex",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
