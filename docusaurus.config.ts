4e796bbc403e9521256f2a8d5e8179871614476d
nazarov <nezerovabuzar@gmail.com>
2026-04-08 19:04:06 +0400

    redirection created

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,14 +18,14 @@ const config: Config = {
     url: "https://2flexible.com/",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/2flex",
+    baseUrl: "/",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: "2flexible", // Usually your GitHub org/user name.
     projectName: "2flexible", // Usually your repo name.
 
-    onBrokenLinks: "throw",
+    onBrokenLinks: "warn",
 
     // Even if you don't use internationalization, you can use this field to set
     // useful metadata like html lang. For example, if your site is Chinese, you
